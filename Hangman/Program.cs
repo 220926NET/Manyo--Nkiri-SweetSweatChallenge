7f72fdf5a7e9685ef44514f5590846afb997549d
Manyo Nkiri <manyoojongnkiri@gmail.com>
2022-10-03 12:59:53 -0700

    Commit

---
+++ Hangman/Program.cs
@@ -0,0 +1,133 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*
+    
+    
+    You should be able guess a letter or a word
+    You should know how many letters are in the word 
+    you should know how many number of tries you have left, you have seven tries
+
+
+    User story:
+     User should be able to guess a word or letter
+     User Should be able to know how may letters are in the word
+     user should be informed if the word or letter they guessed is incorrect
+
+     MVP
+        Users should be able to see the remaining guesses
+            hang man has 6 set a counter for 6 everytime you guess counter decreases by 1
+
+        Users Should be able to quit game
+
+        Users should be able to make a letter guess and see if it is in the word or not
+        Users should see where the correct letters are in the word
+        users should know how many letters in word 
+        Users should be able to guess the word
+        Answer should be a real world
+
+
+
+*/
+
+// Need array of letters that equal word
+//Write guess a letter 
+//Allow user in input
+//Loop thru array of letter words if char user  
+
+using System;
+using System.Collections.Generic;
+
+namespace Almostengr.CodingChallenge.Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hangman!");
+            Console.WriteLine();
+
+            string[] wordList = { "aardvark", "baboon", "camel", "dog", "elephant", "ferret", "giraffe" };
+
+            // todo 1
+            // randomly choose a word from the word list and assign it to a variable called chosenWord
+
+            Random random = new Random();
+            int wordListInt = random.Next(0, wordList.Length - 1);
+
+            string chosenWord = wordList[wordListInt].ToLower();
+            Console.WriteLine("The word has {0} characters", chosenWord.Length);
+
+            int remainingAttempts = 7;
+            List<string> alreadyGuessedLetters = new List<string>();
+            string displayWord = "";
+
+            while (remainingAttempts > 0 && displayWord != chosenWord)
+            {
+                Console.WriteLine("Remaining attempts: {0}", remainingAttempts);
+
+                // todo 2
+                // ask the user to guess a letter and assign their answer to a variable called
+                // "guess". Make guess lowercase.
+
+                Console.WriteLine("Enter a letter: ");
+                char[] input = Console.ReadLine().ToCharArray();
+
+                string guess = "";
+                if (input.Length > 0)
+                {
+                    guess = input[0].ToString();
+                    guess = guess.ToLower();
+                }
+
+                alreadyGuessedLetters.Add(guess);
+
+                // todo 3
+                // check if the letter the user guessed (guess) is one of the letters in the chosenWord
+
+                displayWord = "";
+                string containedMessage = "";
+                if (chosenWord.Contains(guess))
+                {
+                    containedMessage = "{0} is contained in the word.";
+                }
+                else
+                {
+                    containedMessage = "{0} is NOT contained in the word.";
+                    remainingAttempts--;
+                }
+
+                Console.WriteLine(containedMessage, guess);
+
+                foreach (var letter in chosenWord)
+                {
+                    if (alreadyGuessedLetters.Contains(letter.ToString()))
+                    {
+                        displayWord += letter;
+                    }
+                    else
+                    {
+                        displayWord += "*";
+                    }
+                }
+
+                Console.WriteLine(displayWord);
+                Console.WriteLine();
+            } // end while
+
+            if (remainingAttempts == 0)
+            {
+                Console.WriteLine("You have ran out of attempts. You lose!");
+                Console.WriteLine("The word was: {0}", chosenWord);
+            }
+            else
+            {
+                Console.WriteLine("You have won the game!");
+            }
+        }
+    }
+}
+
+
+
+
---
+++ blackjack/Program.cs
@@ -0,0 +1,176 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Welcome to BlackJack");
+Console.WriteLine("How much would you like to bet the min is 10 dollars and the max is 500");
+int bet = Convert.ToInt32(Console.ReadLine());
+
+BlackJack game = new BlackJack(bet);
+game.startGame();
+
+public class BlackJack{
+    public string P1;
+    public string dealer = "house";
+    public int p1score = 0;
+    public int dscore = 0;
+    public int p1Wins = 0;
+    public int dwins = 0;
+    public int round =1;
+    public bool won = false;
+
+    public int p1bet; 
+
+
+    //Constructor
+    public BlackJack(int p1bet)
+    {
+        this.p1bet = p1bet;
+    }
+
+
+    //Main Method
+
+    //static void Main(string[] args){
+
+    //}
+
+
+    //Methods
+    public void startGame(){
+        Console.WriteLine("What is your name");
+        P1 = Console.ReadLine();
+        Console.WriteLine($"\nWelcome {P1} lets go");
+        Console.WriteLine("Objective: Dont get over 21");
+
+
+        while(true)
+        {
+            Random rnd = new Random();
+
+            if(round == 1)
+            {
+            int p1Card = rnd.Next(1,11);
+            int dealerCard = rnd.Next(1,11);
+            p1score = p1score + p1Card;
+            dscore = dscore + dealerCard;
+
+            Console.WriteLine($"{P1} you are dealt a {p1Card}");
+
+
+            Console.WriteLine($"{dealer} is dealt a {dealerCard}\n");
+            round++;
+
+            }
+
+            if(round > 1 && !won)
+            {
+                if(dscore > p1score){
+                    //Console.WriteLine($"{P1} please select another card.");
+                    Console.Clear();
+
+                }
+                else
+                Console.WriteLine("Would you like another card? Hit or Stay");
+                string hitorStay = Console.ReadLine();
+                    
+                if(hitorStay.ToLower() == "hit" && !won)
+                    {
+                        int p1Card = rnd.Next(1,11);
+                        p1score = p1score + p1Card;
+                        Console.WriteLine($"{P1} you are dealt {p1Card} total value: {p1score}\n");
+                        CheckBust(p1score,dscore);
+                        if(won) return;
+                    }
+                else if(round > 1 && hitorStay.ToLower() == "stay" && !won)
+                {
+                        while (dscore <= p1score && p1score >0)
+                        {
+                            int dealerCard = rnd.Next(1,11);
+                            dscore =dscore + dealerCard;
+                            Console.WriteLine($"{dealer} is dealt a {dealerCard} total value {dscore}\n");
+                            CheckBust(p1score, dscore);
+                            if(dscore > p1score)
+                            {
+                                Console.WriteLine($"{dealer} wins! you should stop gambling");
+                                dwins++;
+                        
+                                ContinuePlaying();
+                            }
+                        }
+                }
+                
+            }
+            
+
+
+
+        }
+
+    }
+
+    public void ContinuePlaying()
+    {
+        Console.WriteLine("Continue? Yes/No");
+        string response = Console.ReadLine();
+
+        if(response.ToLower() == "yes")
+        {
+            ResetGame();
+        }
+        else
+        {
+            Console.WriteLine("You sure...");
+            Thread.Sleep(4000);
+            Console.WriteLine("fine");
+            ExitGame();
+        }
+    }
+
+    public void ExitGame()
+    {
+        Console.Clear();
+        Environment.Exit(0);
+
+    }
+
+    public void ResetGame()
+    {
+        p1score = 0;
+        dscore = 0;
+        round = 1;
+        Console.Clear();
+    }
+
+    public void CheckBust(int p1score, int dscore)
+    {
+        if(p1score > 21)
+        {
+            Console.WriteLine($"{P1} you Bust, dealer wins");
+            dwins++;
+        }
+        if(dscore > 21)
+        {
+            Console.WriteLine($"{dealer} Bust, {P1} wins");
+            p1Wins++;
+        }
+
+    }
+
+    /*public int PoolMoney(int p1bet)
+    {
+        if(won)
+        {
+            return p1bet;
+        }else if(p1score == dscore)
+        {
+            Console.WriteLine("Push");
+            return 0;
+        }else if(p1score == 21)
+        {
+            return p1bet * 2;
+        }else
+        {
+            return p1bet - p1bet;
+        }
+    } */
+
+}
